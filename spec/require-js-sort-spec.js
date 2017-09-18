dead8f163244e6043dcc889e3bdfe0bce27b9184
Adam Zlatohlavek <zlatohlavecek@gmail.com>
2017-09-18 22:15:11 +0200

    not matched lines will be taken full

--- spec/require-js-sort-spec.js
+++ spec/require-js-sort-spec.js
@@ -3,10 +3,6 @@
 import RequireJsSort from '../lib/require-js-sort';
 
 // Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
-//
-// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
-// or `fdescribe`). Remove the `f` to unfocus the block.
-
 describe('RequireJsSort', () => {
     let workspaceElement, activationPromise;
     beforeEach(() => {
